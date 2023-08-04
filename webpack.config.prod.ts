3f677f09cf2935b8a838a0b5d6b070080d4ea2d6
Gideon Idoko <iamgideonidoko@gmail.com>
2023-08-04 06:27:52 +0100

    build: copy views during client build

--- webpack.config.prod.ts
+++ webpack.config.prod.ts
@@ -1,12 +1,26 @@
 import { join } from 'path';
 import { merge } from 'webpack-merge';
-import config from './webpack.config';
+import config, { getClientWebpackPath } from './webpack.config';
 import type { Configuration } from 'webpack';
+import CopyWebpackPlugin from 'copy-webpack-plugin';
 
 export default merge<Configuration>(config, {
   mode: 'production',
   output: {
     path: join(__dirname, 'build/client/public'),
   },
-  plugins: [],
+  plugins: [
+    new CopyWebpackPlugin({
+      patterns: [
+        {
+          from: getClientWebpackPath('shared'), // copy from shared to the public dir
+          to: '',
+        },
+        {
+          from: getClientWebpackPath('views'), // copy from shared to the public dir
+          to: '../views',
+        },
+      ],
+    }),
+  ],
 });
