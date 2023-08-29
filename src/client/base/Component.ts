606533c386ce178f6b7c7a489827b3ce72d76cc6
Gideon Idoko <iamgideonidoko@gmail.com>
2023-08-29 03:11:58 +0100

    feat(component): make base component class type customizable

--- src/client/base/Component.ts
+++ src/client/base/Component.ts
@@ -1,31 +1,26 @@
 import AutoBind from 'auto-bind';
+import Prefix from 'prefix';
 
-export default class {
-  protected element: HTMLElement | null;
-  protected elements: Record<string, HTMLElement[]>;
+export default class Component<
+  E extends string = string,
+  EE extends Record<string, string> | object = object,
+  MappedE extends HTMLElement = HTMLElement,
+  MappedEE extends Record<keyof EE, HTMLElement[]> = Record<keyof EE, HTMLElement[]>,
+> {
+  protected element: MappedE | null;
+  protected elements?: MappedEE;
+  protected readonly transformPrefix = Prefix('transform') as 'transform';
 
-  public constructor({
-    element,
-    elements,
-  }: {
-    element: string | HTMLElement;
-    elements?: Record<string, string | HTMLElement>;
-  }) {
+  public constructor({ element, elements }: { element: E; elements?: EE }) {
     AutoBind(this);
-    if (element instanceof window.HTMLElement) {
-      this.element = element;
-    } else {
-      this.element = document.querySelector(element);
-    }
+    this.element = document.querySelector(element);
 
-    this.elements = Object.fromEntries(
-      Object.entries({ ...elements }).map(([key, child]) => {
-        if (child instanceof window.HTMLElement) {
-          return [key, [child]];
-        } else {
+    if (elements) {
+      this.elements = Object.fromEntries(
+        Object.entries({ ...elements }).map(([key, child]) => {
           return [key, document.querySelectorAll(child)];
-        }
-      }),
-    );
+        }),
+      ) as unknown as MappedEE;
+    }
   }
 }
