05852208a398d5b2e3e3d25f5e9b35e11be8799e
Gideon Idoko <iamgideonidoko@gmail.com>
2023-08-29 01:40:48 +0100

    feat(page): add about page script

---
+++ src/client/pages/about/index.ts
@@ -0,0 +1,33 @@
+import Page from '@/c/base/Page';
+
+export default class extends Page {
+  public constructor() {
+    super({
+      classes: {
+        active: 'about--active',
+      },
+
+      element: '.about',
+      elements: {
+        wrapper: '.about__wrapper',
+      },
+    });
+  }
+
+  /**
+   * Animations.
+   */
+  public async show() {
+    if (!this.element || !this.classes) return;
+    this.element.classList.add(this.classes.active);
+
+    return super.show();
+  }
+
+  async hide() {
+    if (!this.element || !this.classes) return;
+    this.element.classList.remove(this.classes.active);
+
+    return super.hide();
+  }
+}
